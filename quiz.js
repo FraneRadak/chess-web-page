2605d766144cededc5877e0b438c5e9e07ef3bc7
FraneRadak <fr47786@unist.hr>
2021-12-21 15:10:26 +0100

    web page

---
+++ quiz.js
@@ -0,0 +1,90 @@
+var questionBank= [
+    {
+        question : 'Trenutni šahovski svijetski prvak zove se:',
+        option : ['Levon Aronian','Magnus Carlsen','Alireza Firouzja','Veselin Topalov'],
+        answer : 'Magnus Carlsen'
+    },
+    {
+        question : 'Gary Kasparov je svoju karijeru zavrsio 2004.godine u gradu:',
+        option : ['Sarajevu','Linaresu','Havani','Wijk ann zee'],
+        answer : 'Linaresu'
+    },
+    {
+        question : 'Gambit je žrtva pješaka u svrhu',
+        option : ['Napada','Pozicione prednosti','Brze rohade','Obrane kralja'],
+        answer : 'Pozicione prednosti'
+    },
+    {
+        question : 'Prvi svijetski prvak bpio je:',
+        option : ['Jose Raul Capablanca','Paul Morphy','Adolf Anderssen','Wiliam Steinz'],
+        answer : 'Wiliam Steinz'
+    },
+    {
+        question : 'Koliko polja ima šahovska ploča?',
+        option : ['128','64','72','56'],
+        answer : '64'
+    }
+]
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+next.addEventListener('click',nextQuestion);
+function backToQuiz(){
+    location.reload();
+}
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+displayQuestion();
